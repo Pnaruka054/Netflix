f7765c46fa67d64d4bc080cdba00300b51615831
pnaruka054 <pnaruka054@gmail.com>
2024-02-12 23:25:01 +0530

    first commit

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
 let target = document.querySelectorAll(".main6div")
 target.forEach(items => {
     items.addEventListener("click", () => {
-        items.classList.toggle("active1")
-        items.querySelector(".i").classList.toggle("i1")
+        for (let i = 0; i < target.length; i++) {
+            if (target[i] != items) {
+                target[i].classList.remove("active1")
+                target[i].querySelector(".i").classList.remove("i1")
+            } else {
+                items.classList.toggle("active1")
+                items.querySelector(".i").classList.toggle("i1")
+            }
+        }
     })
 })
